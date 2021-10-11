c78368979d0eebada0d558e37e2826726b7a857a
Dmitry <skifcha200211@mail.ru>
2021-10-11 15:44:05 +0500

    Game "Life"
    Game "Life" is made on VS Windows Form C#

---
+++ Game 'Life'/live/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace live
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 118);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать жизнь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 117);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Уничтожить жизнь";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(197, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1200, 800);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1462, 977);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Жизнь";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Game 'Life'/live/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+// Create by SKIF4A -> https://github.com/evilSKIF4A
+
+
+namespace live
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Graphics graphics; // глобально объявляем, чтобы отрисовывать графику
+        public bool[,] life; // хранит всю информацию о клетках. То есть живая клетка, пустая и т.д.
+        public int stroki; // нужны чтобы определить размер для массива 
+        public int stolbci;
+
+        public int CountNeigth(int x, int y)
+        {
+            int count = 0;
+            // с помощью двух циклов, можно получить информацию о соседях. То есть отняв от x - 1. получим информацию о соседе слево, а если добавить единицу то информацию о соседе справо
+            for(int i = -1; i < 2; i++)
+            {
+                for(int j = -1; j < 2; j++)
+                {
+                    // получим всю информацию о соседях вокруг
+                    int x0 = (x + i + stroki) % stroki;
+                    int y0 = (y + j + stolbci) % stolbci; // Как будто шар, то есть если координаты 0,0, то надо посмотреть уже с другой стороны карты.
+
+                    bool etokletka = x0 == x && y0 == y; // чтобы мы не считали саму клетку.
+                    bool estsosedkletka = life[x0, y0]; // если сосед живая клетка
+                    if(estsosedkletka && !etokletka)
+                    {
+                        count = count + 1; // количество соседей
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Black); // создаем поле для игры. Оно будет черным
+
+            bool[,] newlife = new bool[stroki, stolbci]; // нужна для того, чтобы создавать новые клетки
+
+
+            for (int x = 0; x < stroki; x++)
+            {
+                for (int y = 0; y < stolbci; y++)
+                {
+                    int neigthCount = CountNeigth(x, y); // количество соседей 
+                    bool nlife = life[x, y]; // есть ли живая клетка по текущим координатам
+
+                    //правила генерирования следующего поколения
+                    if (!nlife && neigthCount == 3)
+                    {
+                        newlife[x, y] = true; // зараждается клетка
+
+                    }else if(nlife && (neigthCount < 2 || neigthCount > 3))
+                    {
+                        newlife[x, y] = false; // клетки не будет
+                    }
+                    else
+                    {
+                        newlife[x, y] = life[x, y]; // если ничего не выполнилось, то все клетки остаются там где нужно
+                    }
+
+                    if (nlife)
+                    {
+                        graphics.FillRectangle(Brushes.Green, x*4, y*4, 4, 4); // отрисовывает все клетки. Размер 4х4. Координаты смещаются в 4 раза,чтобы клетки отрисовывались по всему pictureBox.
+                    }
+                }
+            }
+
+
+            life = newlife; // после всего что выполнится, мы сохраняем все в массив life;
+            pictureBox1.Refresh(); // обновляем pictureBox
+        }
+
+        // старт
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) return; // если таймер включен, то выйти из метода
+
+            stroki = pictureBox1.Width / 4; // делим на 4, чтобы увеличить масштаб 
+            stolbci = pictureBox1.Height / 4;
+            life = new bool[stroki, stolbci];
+
+            Random ran = new Random(); // рандомно создаем первое поколение клеток
+            for(int x = 0; x < stroki; x++)
+            {
+                for(int y = 0; y < stolbci; y++)
+                {
+                    life[x, y] = ran.Next(10) == 0; // рандомно генерируются клетки. Если рандомное число будет 0, то будет true и клетка создаться.
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Enabled = true;
+        }
+
+        // стоп
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                return; // если таймер выключен, то ничего не делать 
+            }
+            else
+            {
+                timer1.Enabled = false;
+                graphics.Clear(Color.Black); // очищаем поле
+                pictureBox1.Refresh(); // обновляем pictureBox
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled) return; // если таймер выключен, чтобы ничего нельзя было делать
+
+            if(e.Button == MouseButtons.Left) // Если левая кнопка нажата, то он создает клетку
+            {
+                int x = e.Location.X / 4; // делим на 4, потому что если не делить, то таких координат не будет в массие life
+                int y = e.Location.Y / 4;
+                if(x >= 0 && y >= 0 && x < stroki && y < stolbci) // условие, чтобы не выходило за границы
+                {
+                    life[x, y] = true;
+                }
+            }
+            if(e.Button == MouseButtons.Right) // Если правая кнопка нажата, то он удаляет клетку
+            {
+                int x = e.Location.X / 4; // делим на 4, потому что если не делить, то таких координат не будет в массие life
+                int y = e.Location.Y / 4;
+                if (x >= 0 && y >= 0 && x < stroki && y < stolbci) // условие, чтобы не выходило за границы
+                {
+                    life[x, y] = false;
+                }
+            }
+        }
+    }
+}
